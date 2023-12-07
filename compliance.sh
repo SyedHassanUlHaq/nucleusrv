10df5aca034095a66558828c341635c7ad071699
SyedHassanUlHaq <21b-029-cs@students.uit.edu>
2023-12-07 23:41:38 +0500

    Initial compliance result

--- compliance.sh
+++ compliance.sh
@@ -1,5 +1,5 @@
 # Disassemble the ELF file
-riscv32-unknown-elf-objdump -D /home/hassan/nucleusrv/imperas-riscv-tests/work/rv32i_m/I/$1.elf > /home/hassan/nucleusrv/imperas_test/$1.disass
+riscv32-unknown-elf-objdump -D /home/hassan/nucleusrv/imperas-riscv-tests/work/rv32i_m/M/$1.elf > /home/hassan/nucleusrv/imperas_test/$1.disass
 
 # Parse the assembly and generate hex file
 python3 /home/hassan/Documents/nucleusrv/tools/trace/scripts/assemblyParser.py --asm /home/hassan/nucleusrv/imperas_test/$1.disass --hex /home/hassan/imperas_test/$1.hex
@@ -21,14 +21,14 @@ make sim IMEM=/home/hassan/nucleusrv/imperas_test/$1.hex
 # Convert sbt dump to log
 python3 /home/hassan/nucleusrv/tools/trace/scripts/sbtToLog.py --asm /home/hassan/nucleusrv/imperas_test/$1.disass --sbt_dump /home/hassan/Documents/nucleusrv/trace.log --log /home/hassan/imperas_test/$1.log
 
-python3 /home/hassan/nucleusrv/imperas_log_to_sig.py --log /home/hassan/nucleusrv/imperas-riscv-tests/work/rv32i_m/I/$1.log --sig /home/hassan/nucleusrv/imperas-riscv-tests/work/	rv32i_m/I/$1.signature
+python3 /home/hassan/nucleusrv/imperas_log_to_sig.py --log /home/hassan/nucleusrv/imperas-riscv-tests/work/rv32i_m/M/$1.log --sig /home/hassan/nucleusrv/imperas-riscv-tests/work/rv32i_m/M/$1.signature
 
 
 	# Convert log to signature
 python3 /home/hassan/nucleusrv/tools/trace/scripts/logToSignature.py --log /home/hassan/nucleusrv/imperas_test/$1.log --disass /home/hassan/nucleusrv/imperas_test/$1.disass --signature /home/hassan/nucleusrv/imperas_test/$1.signature
 
 # Clean the ref signature file
-#python3 /home/hassan/nucleusrv/tools/trace/scripts/hex_cleaner.py --hex /home/hassan/imperas-riscv-tests/work/rv32i_m/I/$1.signature.output
+#python3 /home/hassan/nucleusrv/tools/trace/scripts/hex_cleaner.py --hex /home/hassan/imperas-riscv-tests/work/rv32i_m/M/$1.signature.output
 
 # Signature Compare
-python3 /home/hassan/compare.py --ref_sig /home/hassan/imperas-riscv-tests/work/rv32i_m/I/$1.signature --dut_sig /home/hassan/imperas_test/$1.signature
+python3 /home/hassan/compare.py --ref_sig /home/hassan/imperas-riscv-tests/work/rv32i_m/M/$1.signature --dut_sig /home/hassan/imperas_test/$1.signature
